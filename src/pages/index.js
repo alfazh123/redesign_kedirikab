74a661fc2917dfbc8dadaec090e2aa4904af9255
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-10-18 11:04:41 +0700

    fix landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,147 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Slider from '../component/slider';
+import CardPotensi from '../component/cardPotensi';
+import Footer from '@/component/footer';
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main className={` ${inter.className}`}>
+            <div className="min-h-screen flex flex-col text-center justify-center ">
+                {/* <img
+                    className="absolute inset-0 h-full w-full object-cover"
+                    src="https://4.bp.blogspot.com/-5Mt5bpL1nCk/T_Qc0pNnAxI/AAAAAAAAAI4/8pteJJpclcE/s1600/Kediri_East_Java.jpg"
+                /> */}
+                <Image
+                    className="absolute inset-0 h-full w-full object-cover"
+                    src="/kediri.png"
+                    alt="Picture of the author"
+                    layout="fill"
+                    objectFit="cover"
+                    quality={100}
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-cyan-700 from-5%  to-slate-300 opacity-90"></div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <h1 className="flex h-full relative items-center justify-center text-6xl font-bold text-white">
+                    Pemkab Kediri
+                </h1>
+                <ul className="flex text-center items-center mx-auto justify-center max-w-md relative mt-6">
+                    <li className="md:mx-4 sm:mx-1 mx-1">
+                        <Link href="/berita">
+                            <button className="btn btn-outline text-white hover:bg-white hover:text-gray-700">
+                                Berita
+                            </button>
+                        </Link>
+                    </li>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                    <li className="md:mx-4 sm:mx-1 mx-1">
+                        <Link href="/profil">
+                            <button className="btn btn-outline text-white hover:bg-white hover:text-gray-700">
+                                Profil
+                            </button>
+                        </Link>
+                    </li>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                    <li className="md:mx-4 sm:mx-1 mx-1">
+                        <Link href="/fasilitas">
+                            <button className="btn btn-outline text-white hover:bg-white hover:text-gray-700">
+                                Fasilitas
+                            </button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+            <div className="bg-gradient-to-b from-cyan-700 to-slate-300 ">
+                <div className="">
+                    <div className="flex flex-col items-center justify-center text-slate-100">
+                        <h1 className="text-4xl font-bold text-center mt-20">
+                            Berita Terbaru
+                        </h1>
+                        <p className="text-sm text-center max-w-md">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Quisquam, voluptatibus.
+                        </p>
+                    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                    <div className="flex flex-col items-center mx-10 ">
+                        <ul className="flex lg:flex-row flex-col">
+                            <li className="my-5">
+                                <div className="card md:w-92 lg:w-62 lg:min-h-52 md:mx-4 w-64 shadow-xl sm:w-72 bg-white">
+                                    <figure>
+                                        <img
+                                            src="https://berita.kedirikab.go.id/asset/foto_berita/KPK_(3).jpeg"
+                                            alt="Shoes"
+                                        />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h3 className="card-title font-medium text-black">
+                                            KPK RI Gelar Sosialisasi Anti
+                                            Korupsi
+                                        </h3>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li className="my-5">
+                                <div className="card md:w-92 lg:w-62 lg:min-h-52 md:mx-4 w-64 bg-white shadow-xl sm:w-72">
+                                    <figure>
+                                        <img
+                                            src="https://berita.kedirikab.go.id/asset/foto_berita/bpsdm_(2).jpeg"
+                                            alt="Shoes"
+                                        />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h3 className="card-title font-medium text-black">
+                                            Antisipasi Dampak Kekeringan,
+                                            Gubernur Khofifah Harap Kepala
+                                            Daerah Upayakan Mitigasi Bencana
+                                        </h3>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li className="my-5">
+                                <div className="card md:w-92 lg:w-62 lg:min-h-52 md:mx-4 w-64 bg-white shadow-xl sm:w-72">
+                                    <figure>
+                                        <img
+                                            src="https://berita.kedirikab.go.id/asset/foto_berita/mangga_(3).jpeg"
+                                            alt="Shoes"
+                                        />
+                                    </figure>
+                                    <div className="card-body">
+                                        <h3 className="card-title font-medium text-black">
+                                            Pesan Mas Dhito Bagi Petani Mangga
+                                            Podang: Jangan Di Booster
+                                        </h3>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                {/*potensi section*/}
+                <div className="">
+                    <div className=" px-20 pt-20 flex flex-col items-center justify-center text-slate-800">
+                        <h1 className="text-4xl font-bold text-center mt-5 bg-teal-">
+                            Potensi Daerah
+                        </h1>
+                        <p className="text-sm text-center max-w-md">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Quisquam, voluptatibus.
+                        </p>
+                    </div>
+                    <div className="flex item-center justify-center md:p-10">
+                        <Slider className="bg-slate-100" />
+                    </div>
+                </div>
+            </div>
+
+            <Footer />
+        </main>
+    );
 }
