7019d75c2861815bf8c6ada78bf7ec013b8739a2
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-10-25 17:49:15 +0700

    fixing potensi page

--- src/component/cardPotensi.js
+++ src/component/cardPotensi.js
@@ -1,7 +1,7 @@
 export default function CardPotensi({ title, description, number }) {
     return (
         <div>
-            <div className="card  md:w-90 xl:w-90 lg:w-90 sm:w-64  bg-slate-100  text-slate-800 rounded-none">
+            <div className="card md:w-90 xl:w-90 lg:w-90 sm:w-60  bg-slate-100  text-slate-800 rounded-none">
                 <div className="card-body ">
                     <h2 className="card-title ">{title}</h2>
                     <p>{description}</p>
