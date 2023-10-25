0cc0a08b32057b1b5673bcc5e251858f3cdaceb3
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-10-25 17:49:35 +0700

    ficxing lapor page in date picker

--- src/component/datePicker.js
+++ src/component/datePicker.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Datetime from 'react-datetime';
+import { AiOutlineCalendar } from 'react-icons/ai';
 import 'react-datetime/css/react-datetime.css';
 
 export default function DatePicker() {
     return (
         <div>
-            <Datetime className="appearance-none shadow border rounded-md py-3 px-2 text-gray-darker w-full" />
+            <Datetime className="appearance-none border mx-2 py-2 text-gray-darker" />
         </div>
     );
 }
